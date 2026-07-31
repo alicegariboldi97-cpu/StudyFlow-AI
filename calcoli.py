86f695be4763e045a92f6a4acd72917fef645cb6
alicegariboldi97-cpu <alice.gariboldi97@gmail.com>
2026-07-31 12:49:11 +0200

    Update calcoli.py

--- calcoli.py
+++ calcoli.py
@@ -1,18 +1,26 @@
 #==========================================
-#calcolo del carico di studio
+# CALCOLO DEL CARICO DI STUDIO
 #==========================================
 
-ore_per_cfu=15
-esami=[]
-for nome_esame, cfu in cfu_esami.items():
-  ore_studio=cfu*ore_per_cfu
-  giorni_base=ore_studio/media_studio_ore_giornaliere
-  giorni_preparazione=round(giorni_base*1.5)
-  esame={"nome": nome_esame,
-         "cfu": cfu,
-         "ore_studio": ore_studio,
-         "giorni_base": giorni_base,
-         "giorni_preparazione": giorni_preparazione}
-  esami.append(esame)
-for esame in esami:
-  print(esame)
+def calcola_carico_studio(cfu_esami, media_studio_ore_giornaliere):
+
+    ore_per_cfu = 15
+    esami = []
+
+    for nome_esame, cfu in cfu_esami.items():
+
+        ore_studio = cfu * ore_per_cfu
+        giorni_base = ore_studio / media_studio_ore_giornaliere
+        giorni_preparazione = round(giorni_base * 1.5)
+
+        esame = {
+            "nome": nome_esame,
+            "cfu": cfu,
+            "ore_studio": ore_studio,
+            "giorni_base": giorni_base,
+            "giorni_preparazione": giorni_preparazione
+        }
+
+        esami.append(esame)
+
+    return esami
