099e949a714a9eee2ee244ab19c9826e3e43ae6b
alicegariboldi97-cpu <alice.gariboldi97@gmail.com>
2026-07-31 12:44:28 +0200

    planner.py

--- planner.py
+++ planner.py
@@ -28,6 +28,96 @@ for esame in esami:
 
     if scelta is None:
         scelta = datetime.strptime(date_esame[0], "%d/%m/%Y")
+
+    #==========================================
+#ricalcolo piano
+#==========================================
+
+from datetime import datetime
+
+mese, anno = obiettivo_fine.split("/")
+
+data_limite = datetime(int(anno), int(mese), 28)
+
+
+for esame in esami:
+
+    punteggio_migliore = -1
+    data_migliore = None
+
+    for data in esame["date_disponibili"]:
+
+        data_appello = datetime.strptime(data, "%d/%m/%Y")
+
+        if data_appello > data_limite:
+            continue
+
+        punteggio = 0
+
+        giorni_disponibili = (data_appello - datetime.today()).days
+
+        if giorni_disponibili >= esame["giorni_preparazione"]:
+            punteggio += 50
+        else:
+            punteggio -= 30
+
+        if esame["cfu"] >= 9 and giorni_disponibili > 60:
+            punteggio += 30
+
+        elif esame["cfu"] == 6 and giorni_disponibili > 30:
+            punteggio += 20
+
+        for altro_esame in esami:
+
+            if "data_consigliata" in altro_esame:
+
+                altra_data = datetime.strptime(altro_esame["data_consigliata"], "%d/%m/%Y")
+
+                distanza = abs((data_appello - altra_data).days)
+
+                if distanza < 14:
+                    punteggio -= 40
+
+        if punteggio > punteggio_migliore:
+
+            punteggio_migliore = punteggio
+            data_migliore = data_appello
+
+
+    if data_migliore:
+
+        esame["data_consigliata"] = data_migliore.strftime("%d/%m/%Y")
+        esame["tipo_piano"] = "Distribuito"
+
+    else:
+
+        date_valide = []
+
+        for data in esame["date_disponibili"]:
+
+            data_appello = datetime.strptime(data, "%d/%m/%Y")
+
+            if data_appello <= data_limite:
+                date_valide.append(data_appello)
+
+        if date_valide:
+
+            data_migliore = max(date_valide)
+
+            esame["data_consigliata"] = data_migliore.strftime("%d/%m/%Y")
+            esame["tipo_piano"] = "Sessione ravvicinata"
+
+        else:
+
+            esame["data_consigliata"] = "Nessuna data disponibile"
+            esame["tipo_piano"] = "Oltre obiettivo"
+
+
+print("\nPIANO ESAMI OTTIMIZZATO\n")
+
+for esame in sorted(esami, key=lambda x: datetime.strptime(x["data_consigliata"], "%d/%m/%Y") if x["data_consigliata"] != "Nessuna data disponibile" else datetime.max):
+
+    print(esame["nome"], "|", esame["cfu"], "CFU |", esame["data_consigliata"], "|", esame["tipo_piano"])
         esame["tipo_piano"] = "Sessione ravvicinata"
     else:
         esame["tipo_piano"] = "Distribuito"
