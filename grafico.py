b875956c10d6040bd86c8070b2fc3d807436c06d
alicegariboldi97-cpu <alice.gariboldi97@gmail.com>
2026-07-31 13:51:04 +0200

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -13,49 +13,141 @@ from datetime import datetime
 
 def crea_grafico(esami):
 
+    esami_validi = []
+
+
+    for esame in esami:
+
+        if esame["data_consigliata"] != "Nessuna data disponibile":
+
+            esami_validi.append(esame)
+
+
+
     esami_ordinati = sorted(
-        esami,
-        key=lambda x: datetime.strptime(x["inizio_studio"], "%d/%m/%Y")
+        esami_validi,
+        key=lambda x: datetime.strptime(
+            x["inizio_studio"],
+            "%d/%m/%Y"
+        )
     )
 
+
+
     nomi = []
+
     inizi = []
+
     durate = []
+
     date_esame = []
+
+
 
     for esame in esami_ordinati:
 
-        inizio = datetime.strptime(esame["inizio_studio"], "%d/%m/%Y")
-        fine = datetime.strptime(esame["data_consigliata"], "%d/%m/%Y")
 
-        nomi.append(esame["nome"].replace("_", " "))
-        inizi.append(inizio)
-        durate.append((fine - inizio).days)
-        date_esame.append(fine)
+        inizio = datetime.strptime(
+            esame["inizio_studio"],
+            "%d/%m/%Y"
+        )
 
 
-    fig, ax = plt.subplots(figsize=(12, 7))
+        fine = datetime.strptime(
+            esame["data_consigliata"],
+            "%d/%m/%Y"
+        )
+
+
+        nomi.append(
+            esame["nome"].replace("_", " ")
+        )
+
+
+        inizi.append(
+            inizio
+        )
+
+
+        durate.append(
+            (fine - inizio).days
+        )
+
+
+        date_esame.append(
+            fine
+        )
+
+
+
+    fig, ax = plt.subplots(
+        figsize=(12, 7)
+    )
+
 
 
     for i in range(len(nomi)):
 
-        ax.barh(i, durate[i], left=inizi[i], height=0.5)
-        ax.axvline(date_esame[i], linewidth=1)
+        ax.barh(
+            i,
+            durate[i],
+            left=inizi[i],
+            height=0.5
+        )
 
 
-    ax.set_yticks(range(len(nomi)))
-    ax.set_yticklabels(nomi)
+        ax.axvline(
+            date_esame[i],
+            linewidth=1
+        )
 
-    ax.xaxis.set_major_locator(mdates.MonthLocator())
-    ax.xaxis.set_major_formatter(mdates.DateFormatter("%m/%Y"))
 
-    plt.xticks(rotation=45)
 
-    ax.set_title("Piano annuale di studio")
-    ax.set_xlabel("Periodo di preparazione")
-    ax.set_ylabel("Esami")
+    ax.set_yticks(
+        range(len(nomi))
+    )
 
-    ax.grid(axis="x")
+
+    ax.set_yticklabels(
+        nomi
+    )
+
+
+    ax.xaxis.set_major_locator(
+        mdates.MonthLocator()
+    )
+
+
+    ax.xaxis.set_major_formatter(
+        mdates.DateFormatter("%m/%Y")
+    )
+
+
+    plt.xticks(
+        rotation=45
+    )
+
+
+    ax.set_title(
+        "Piano annuale di studio"
+    )
+
+
+    ax.set_xlabel(
+        "Periodo di preparazione"
+    )
+
+
+    ax.set_ylabel(
+        "Esami"
+    )
+
+
+    ax.grid(
+        axis="x"
+    )
+
 
     plt.tight_layout()
+
     plt.show()
