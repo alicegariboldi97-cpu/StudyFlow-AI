59856e3f34e386a9d4c806b10fbd1f73763713a4
alicegariboldi97-cpu <alice.gariboldi97@gmail.com>
2026-07-31 14:55:23 +0200

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -1,62 +1,70 @@
-from datetime import datetime
 import matplotlib.pyplot as plt
+from datetime import datetime
 
 
 def crea_grafico(esami):
 
-    esami_validi = []
+    print("\nCreazione grafico StudyFlow AI...")
+
+    nomi = []
+    date_inizio = []
+    durate = []
+
 
     for esame in esami:
 
-        if (
-            "data_consigliata" not in esame
-            or esame["data_consigliata"] == "Nessuna data disponibile"
-        ):
+        if esame.get("data_consigliata") in [
+            None,
+            "Nessuna data disponibile"
+        ]:
             continue
 
         if "inizio_studio" not in esame:
             continue
 
-        esami_validi.append(esame)
+
+        try:
+
+            inizio = datetime.strptime(
+                esame["inizio_studio"],
+                "%d/%m/%Y"
+            )
+
+            fine = datetime.strptime(
+                esame["data_consigliata"],
+                "%d/%m/%Y"
+            )
 
 
-    if not esami_validi:
-        print("Nessun dato disponibile per il grafico")
+            giorni = (fine - inizio).days
+
+
+            if giorni <= 0:
+                giorni = 1
+
+
+            nomi.append(esame["nome"])
+            date_inizio.append(inizio)
+            durate.append(giorni)
+
+
+        except Exception as e:
+
+            print(
+                "Errore grafico per",
+                esame.get("nome"),
+                ":",
+                e
+            )
+
+
+    if len(nomi) == 0:
+
+        print(
+            "Nessun esame disponibile per il grafico"
+        )
+
         return
-
-
-    esami_ordinati = sorted(
-        esami_validi,
-        key=lambda x: datetime.strptime(
-            x["inizio_studio"],
-            "%d/%m/%Y"
-        )
-    )
-
-
-    nomi = []
-    inizi = []
-    durate = []
-
-
-    for esame in esami_ordinati:
-
-        inizio = datetime.strptime(
-            esame["inizio_studio"],
-            "%d/%m/%Y"
-        )
-
-        fine = datetime.strptime(
-            esame["data_consigliata"],
-            "%d/%m/%Y"
-        )
-
-        giorni = (fine - inizio).days
-
-
-        nomi.append(esame["nome"])
-        inizi.append(inizio)
-        durate.append(giorni)
 
 
 
@@ -66,15 +74,31 @@ def crea_grafico(esami):
     ax.barh(
         nomi,
         durate,
-        left=inizi
+        left=date_inizio
     )
 
 
-    ax.set_xlabel("Periodo di studio")
-    ax.set_ylabel("Esami")
-    ax.set_title("Piano di studio StudyFlow AI")
+    ax.set_title(
+        "Piano di studio StudyFlow AI"
+    )
 
+    ax.set_xlabel(
+        "Periodo di preparazione"
+    )
+
+    ax.set_ylabel(
+        "Esami"
+    )
+
+
+    plt.xticks(rotation=45)
 
     plt.tight_layout()
 
+
+    # MOSTRA IL GRAFICO IN COLAB
     plt.show()
+
+
+    # evita che Colab mostri solo Figure(...)
+    return fig
