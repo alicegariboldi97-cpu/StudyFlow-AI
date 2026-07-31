c91f63cb01002a13c60d0645a76df2d0233f311a
alicegariboldi97-cpu <alice.gariboldi97@gmail.com>
2026-07-31 12:39:50 +0200

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,29 @@
+#==========================================
+#benvenuto
+#==========================================
 
+print("Benvenuti in Studyflow AI",
+      "Ti aiuterà a organizzare le tue sessioni")
+#==========================================
+#dati personali
+#==========================================
+
+nome="Alice"
+universita="Ecampus"
+media_studio_ore_giornaliere=4
+inizio_studio="10-08-2026"
+print(nome)
+print(universita)
+print(media_studio_ore_giornaliere)
+print(inizio_studio)
+
+#cfu e nome esame#
+
+cfu_esami = {"neuropsicologia": 6,
+            "psichiatria" : 9,
+            "psicologia_fisiologica_e_delle_emozioni": 9,
+            "psicometria": 6,
+            "psicologia_dello_sviluppo_tipico_e_atipico": 9,
+            "filosofia_della_mente": 9,
+            "psicologia_clinica":9}
+print (cfu_esami)
