c02168d2b371c6469aa672be8b32d9823a7ca29f
alicegariboldi97-cpu <alice.gariboldi97@gmail.com>
2026-07-31 13:06:41 +0200

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -1,100 +1,61 @@
-#==========================================
-#visualizzazione del piano
-#==========================================
+# ==========================================================
+# IMPORTAZIONE LIBRERIE
+# ==========================================================
 
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from datetime import datetime
 
-esami_ordinati = sorted(esami, key=lambda x: datetime.strptime(x["inizio_studio"], "%d/%m/%Y"))
 
-nomi = []
-inizi = []
-durate = []
-date_esame = []
+# ==========================================================
+# CREAZIONE GRAFICO PIANO DI STUDIO
+# ==========================================================
 
-for esame in esami_ordinati:
-    inizio = datetime.strptime(esame["inizio_studio"], "%d/%m/%Y")
-    fine = datetime.strptime(esame["data_consigliata"], "%d/%m/%Y")
+def crea_grafico(esami):
 
-    nomi.append(esame["nome"].replace("_", " "))
-    inizi.append(inizio)
-    durate.append((fine - inizio).days)
-    date_esame.append(fine)
+    esami_ordinati = sorted(
+        esami,
+        key=lambda x: datetime.strptime(x["inizio_studio"], "%d/%m/%Y")
+    )
 
-fig, ax = plt.subplots(figsize=(12, 7))
+    nomi = []
+    inizi = []
+    durate = []
+    date_esame = []
 
-for i in range(len(nomi)):
-    ax.barh(i, durate[i], left=inizi[i], height=0.5)
-    ax.axvline(date_esame[i], linewidth=1)
+    for esame in esami_ordinati:
 
-#==========================================
-#nuova visualizzazione
-#==========================================
+        inizio = datetime.strptime(esame["inizio_studio"], "%d/%m/%Y")
+        fine = datetime.strptime(esame["data_consigliata"], "%d/%m/%Y")
 
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-from datetime import datetime
-
-esami_ordinati = sorted(esami, key=lambda x: datetime.strptime(x["inizio_studio"], "%d/%m/%Y"))
-
-nomi = []
-inizi = []
-durate = []
-
-for esame in esami_ordinati:
-
-    inizio = datetime.strptime(esame["inizio_studio"], "%d/%m/%Y")
-
-    fine = datetime.strptime(esame["data_consigliata"], "%d/%m/%Y")
-
-    nomi.append(esame["nome"].replace("_", " "))
-
-    inizi.append(inizio)
-
-    durate.append((fine - inizio).days)
+        nomi.append(esame["nome"].replace("_", " "))
+        inizi.append(inizio)
+        durate.append((fine - inizio).days)
+        date_esame.append(fine)
 
 
-fig, ax = plt.subplots(figsize=(12, 7))
-
-for i in range(len(nomi)):
-
-    ax.barh(i, durate[i], left=inizi[i], height=0.5)
+    fig, ax = plt.subplots(figsize=(12, 7))
 
 
-ax.set_yticks(range(len(nomi)))
+    for i in range(len(nomi)):
 
-ax.set_yticklabels(nomi)
+        ax.barh(i, durate[i], left=inizi[i], height=0.5)
+        ax.axvline(date_esame[i], linewidth=1)
 
-ax.xaxis.set_major_locator(mdates.MonthLocator())
 
-ax.xaxis.set_major_formatter(mdates.DateFormatter("%m/%Y"))
+    ax.set_yticks(range(len(nomi)))
+    ax.set_yticklabels(nomi)
 
-plt.xticks(rotation=45)
+    ax.xaxis.set_major_locator(mdates.MonthLocator())
+    ax.xaxis.set_major_formatter(mdates.DateFormatter("%m/%Y"))
 
-ax.set_title("Piano annuale di studio")
+    plt.xticks(rotation=45)
 
-ax.set_xlabel("Periodo di preparazione")
+    ax.set_title("Piano annuale di studio")
+    ax.set_xlabel("Periodo di preparazione")
+    ax.set_ylabel("Esami")
 
-ax.set_ylabel("Esami")
+    ax.grid(axis="x")
 
-ax.grid(axis="x")
-
-plt.tight_layout()
-
-plt.show()
-ax.set_yticks(range(len(nomi)))
-ax.set_yticklabels(nomi)
-ax.xaxis.set_major_locator(mdates.MonthLocator())
-ax.xaxis.set_major_formatter(mdates.DateFormatter("%m/%Y"))
-
-plt.xticks(rotation=45)
-
-ax.set_title("Piano annuale di studio")
-ax.set_xlabel("Periodo di preparazione")
-ax.set_ylabel("Esami")
-
-ax.grid(axis="x")
-
-plt.tight_layout()
-plt.show()
+    plt.tight_layout()
+    plt.show()
